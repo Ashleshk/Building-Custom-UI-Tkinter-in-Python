eea92612feaa8c8debc9d0d374dbc0faa54a9afb
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-29 14:39:22 +0530

    Intoduction Code

---
+++ Introduction.py
@@ -0,0 +1,12 @@
+# BASIC WINDOW  IMPLEMENTATION
+
+
+from tkinter import *
+
+root =Tk()               #the root window
+theLabel = Label(root,text="This is Too EAsy")   
+theLabel.pack()         # put it somewhere in window
+root.mainloop()   # for conti dislay
+
+# STEPS TO BUILD
+# !. organise the Layout
