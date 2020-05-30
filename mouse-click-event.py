ea986c5052f58aab9f1b2e35f7eba7339b1b0bdb
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-30 14:20:11 +0530

    Mouse Click Event handling

---
+++ mouse-click-event.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+root =Tk()
+
+def leftClick(event):
+    print("Left")
+def middleClick(event):
+    print("middle")
+def rightClick(event):
+    print("right")
+
+frame = Frame(root,width=300,height=250)
+frame.bind("<Button-1>",leftClick)
+frame.bind("<Button-3>",rightClick)
+frame.pack()
+
+root.mainloop()
