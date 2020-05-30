09e6958bffda47355dd7c2575dc62465905a9c34
Ashlesh Khajbage <ashleshuk@gmail.com>
2020-05-30 14:18:14 +0530

    Creating Drop Down

---
+++ creting-drop-down.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+def doNothing():
+    print("fanid dnjda")    
+
+
+root=Tk()
+
+# *******  MENU ************
+menu = Menu(root)
+root.config(menu=menu)
+
+subMenu =Menu(menu)
+menu.add_cascade(label="file",menu=subMenu)
+subMenu.add_command(label="new Project...",command=doNothing)
+subMenu.add_command(label="New..",command=doNothing)
+subMenu.add_separator()
+subMenu.add_command(label="Exit",command=doNothing)
+
+editMenu =Menu(menu)
+menu.add_cascade(label="Edit",menu=editMenu)
+editMenu.add_command(label="Redo",command=doNothing)
+editMenu.add_command(label="endo",command=doNothing)
+editMenu.add_separator()
+editMenu.add_command(label="view",command=doNothing)
+
+## *********** TOOLBAR ***********************
+toolbar =Frame(root,bg="blue")
+insertButt =Button(toolbar,text="Insert Image",command=doNothing)
+insertButt.pack(side=LEFT,padx=2,pady=2)
+printButt =Button(toolbar,text="Print",command=doNothing)
+printButt.pack(side=LEFT,padx=2,pady=2)
+
+toolbar.pack(side=TOP,fill=X)
+
+## ************ Status Bar *********************
+status =Label(root,text="Preparing to do nothing",bd=1,relief=SUNKEN,anchor=W)
+status.pack(side=BOTTOM,fill=X)
+
+
+
+
+
+root.mainloop()
